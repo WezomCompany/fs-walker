eb19197e38448a78c5fec7033b61b4f9d3ff2237
OlehDutchenko <dutchenko.o.dev@gmail.com>
2023-09-03 12:41:36 +0300

    getFile

--- src/FsWalker.ts
+++ src/FsWalker.ts
@@ -2,11 +2,11 @@ import { glob } from 'glob';
 import { FileSource } from './FileSource';
 
 export class FsWalker {
-	static makeFile(source: string): FileSource {
+	static getFile(source: string): FileSource {
 		return new FileSource(source);
 	}
 
-	static makeFilesFromGlob(pattern: string): FileSource[] {
+	static getFilesByMask(pattern: string): FileSource[] {
 		return glob
 			.sync(pattern)
 			.map((source: string) => new FileSource(source));
--- src/__specs__/FsWalker.spec.ts
+++ src/__specs__/FsWalker.spec.ts
@@ -5,17 +5,17 @@ import { FileSource } from '../FileSource';
 import { FsWalker } from '../FsWalker';
 
 describe('FsWalker', () => {
+	it('Should return the FileSource', () => {
+		const file = FsWalker.getFile('src/__specs__/fixtures/glob-1/f1.md');
+		expect(file.readAsText().trim()).toBe('# F1');
+	});
+
 	it('Should read search pattern and return array of the FileSource', () => {
-		const sources = FsWalker.makeFilesFromGlob('src/__specs__/fixtures/glob-1/*.{md,mdx}');
+		const sources = FsWalker.getFilesByMask('src/__specs__/fixtures/glob-1/*.{md,mdx}');
 		const allIsFileSource = sources.every((source) => {
 			return expect(source).toBeInstanceOf(FileSource);
 		});
 		expect(sources).toHaveLength(4);
 		expect(allIsFileSource).toBe(true);
-	});
-
-	it('Should return the FileSource', () => {
-		const file = FsWalker.makeFile('src/__specs__/fixtures/glob-1/f1.md');
-		expect(file.readAsText().trim()).toBe('# F1');
 	});
 });
